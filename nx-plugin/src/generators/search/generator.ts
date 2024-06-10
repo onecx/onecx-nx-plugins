a6ced091ab19129b146a3b44f3bfdc30e316a895
markuczy <129275100+markuczy@users.noreply.github.com>
2024-06-10 13:49:03 +0200

    feat: search harnesses with search component tests (#50)
    * feat: search feature harness and tests added
    
    * fix: new pageHeader harness fix
    
    * refactor: search harness naming
    
    * refactor: overflowAction  adjustment in pageHeader harness
    
    * fix: moved details events test to generator

--- nx-plugin/src/generators/search/generator.ts
+++ nx-plugin/src/generators/search/generator.ts
@@ -100,6 +100,7 @@ function addDetailsEventsToSearch(tree: Tree, options: SearchGeneratorSchema) {
     adaptSearchComponent(tree, options);
     adaptSearchActions(tree, options);
     adaptSearchEffects(tree, options);
+    adaptSearchTests(tree, options);
   }
 }
 
@@ -178,6 +179,60 @@ function adaptSearchEffects(tree: Tree, options: SearchGeneratorSchema) {
     
     searchByUrl$`
     );
+  tree.write(filePath, htmlContent);
+}
+
+function adaptSearchTests(tree: Tree, options: SearchGeneratorSchema) {
+  const fileName = names(options.featureName).fileName;
+  const className = names(options.featureName).className;
+  const propertyName = names(options.featureName).propertyName;
+  const filePath = `src/app/${fileName}/pages/${fileName}-search/${fileName}-search.component.spec.ts`;
+
+  let htmlContent = tree.read(filePath, 'utf8');
+  htmlContent = htmlContent.replace(
+    "it('should export csv data on export action click'",
+    `
+    it('should dispatch detailsButtonClicked action on item details click', async () => {
+      jest.spyOn(store, 'dispatch');
+  
+      store.overrideSelector(select${className}SearchViewModel, {
+        ...base${className}SearchViewModel,
+        results: [
+          {
+            id: '1',
+            imagePath: '',
+            column_1: 'val_1',
+          },
+        ],
+        columns: [
+          {
+            columnType: ColumnType.STRING,
+            nameKey: 'COLUMN_KEY',
+            id: 'column_1',
+          },
+        ],
+      });
+      store.refreshState();
+  
+      const interactiveDataView =
+        await ${propertyName}Search.getSearchResults();
+      const dataView = await interactiveDataView.getDataView();
+      const dataTable = await dataView.getDataTable();
+      const rowActionButtons = await dataTable.getActionButtons();
+  
+      expect(rowActionButtons.length).toEqual(1);
+      expect(await rowActionButtons[0].getAttribute('ng-reflect-icon')).toEqual(
+        'pi pi-eye'
+      );
+      await rowActionButtons[0].click();
+  
+      expect(store.dispatch).toHaveBeenCalledWith(
+        ${className}SearchActions.detailsButtonClicked({ id: '1' })
+      );
+    });
+
+    it('should export csv data on export action click'`
+  );
   tree.write(filePath, htmlContent);
 }
 
