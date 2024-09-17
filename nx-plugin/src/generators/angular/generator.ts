09c441539e322b94fc3739d8c28d6b829060db94
Bastian Jakobi <55296998+bastianjakobi@users.noreply.github.com>
2024-09-17 11:48:36 +0200

    fix: switch to latest oneCXLibVersion to fix test issues

--- nx-plugin/src/generators/angular/generator.ts
+++ nx-plugin/src/generators/angular/generator.ts
@@ -75,7 +75,7 @@ export async function angularGenerator(
     tree.delete(`${directory}/scripts/load-permissions.sh`);
   }
 
-  const oneCXLibVersion = '^5.5.0';
+  const oneCXLibVersion = '^5.11.0';
   addDependenciesToPackageJson(
     tree,
     {
