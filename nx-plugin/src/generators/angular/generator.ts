44ab199144ac085e4cf85d7b2a5ad41d38422c89
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2024-09-23 14:37:01 +0200

    refactor: increase oneCXLibVersion

--- nx-plugin/src/generators/angular/generator.ts
+++ nx-plugin/src/generators/angular/generator.ts
@@ -75,7 +75,7 @@ export async function angularGenerator(
     tree.delete(`${directory}/scripts/load-permissions.sh`);
   }
 
-  const oneCXLibVersion = '^5.5.0';
+  const oneCXLibVersion = '^5.12.1';
   addDependenciesToPackageJson(
     tree,
     {
