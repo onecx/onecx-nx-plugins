35d433ca70a902273bbbc2510621ef9d64761fff
jufiedle <juliane.fiedler@capgemini.com>
2024-10-16 12:19:30 +0200

    app reducer step

---
+++ nx-plugin/src/generators/search/steps/app-reducer.step.ts
@@ -0,0 +1,77 @@
+import { Tree, joinPathFragments } from '@nx/devkit';
+import { GeneratorStep } from '../../shared/generator.utils';
+import { SearchGeneratorSchema } from '../schema';
+
+export class AppReducerStep implements GeneratorStep<SearchGeneratorSchema> {
+  //@ts-eslint:ignore @typescript-eslint/no-unused-var
+  process(tree: Tree, _options: SearchGeneratorSchema): void {
+    const reducerFilePath = joinPathFragments('src/app/app.reducers.ts');
+    let reducerContent = tree.read(reducerFilePath, 'utf8');
+
+    if (
+      reducerContent.includes(
+        `import { ActionReducerMap, MetaReducer } from '@ngrx/store';`
+      )
+    ) {
+      reducerContent = reducerContent.replace(
+        `import { ActionReducerMap, MetaReducer } from '@ngrx/store';`,
+        `import { ActionReducerMap, MetaReducer, ActionReducer } from '@ngrx/store';`
+      );
+    }
+
+    if (
+      !reducerContent.includes(
+        `import { localStorageSync } from 'ngrx-store-localstorage';`
+      )
+    ) {
+      reducerContent = reducerContent.replace(
+        `import { State } from './app.state';`,
+        `import { State } from './app.state';
+          import { localStorageSync } from 'ngrx-store-localstorage';
+          import { lazyLoadingMergeReducer } from '@onecx/ngrx-accelerator';`
+      );
+    }
+
+    if (
+      reducerContent.includes(
+        `export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];`
+      )
+    ) {
+      reducerContent = reducerContent.replace(
+        `export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];`,
+        `export function localStorageSyncReducer(
+            reducer: ActionReducer<any>,
+          ): ActionReducer<any> {
+            return localStorageSync({
+              keys: [
+                {
+                  timeslotGroup: [
+                    {
+                      search: [
+                        'chartVisible',
+                        'resultComponentState',
+                        'searchHeaderComponentState',
+                        'diagramComponentState',
+                      ],
+                    },
+                  ],
+                },
+              ],
+              mergeReducer: lazyLoadingMergeReducer,
+              rehydrate: true,
+              storageKeySerializer: (key) => 'ibt-tsg-mgmt-page.\${key}',
+            })(reducer);
+          }
+
+          export const metaReducers: MetaReducer<State>[] = isDevMode()
+            ? [localStorageSyncReducer]
+            : [localStorageSyncReducer];`
+      );
+    }
+    tree.write(reducerFilePath, reducerContent);
+  }
+
+  getTitle(): string {
+    return 'Adapting App Reducer';
+  }
+}
