4be7287e46c5c6a2c0bec676ef0ff5b1e2783500
Benjamin <166110368+BenjaminPabst@users.noreply.github.com>
2025-01-16 15:52:49 +0100

    feat: optimized copy-build to ignore comments (#250)

--- copy-build.js
+++ copy-build.js
@@ -8,19 +8,27 @@ const copyBuildToFile = path.join(__dirname, '.copy-build-to');
 // Check if the .copy-build-to file exists
 if (!fs.existsSync(copyBuildToFile)) {
   // Create the .copy-build-to file with instructions
-  fs.writeFileSync(copyBuildToFile, 'Enter the paths where you want to copy the compiled output, each on a new line.\nExample:\n/path/to/destination1/node_modules/@onecx\n/path/to/destination2/node_modules/@onecx\n');
-  console.log('.copy-build-to file created. Please add the paths where you want to copy the compiled output, each on a new line.');
+  fs.writeFileSync(
+    copyBuildToFile,
+    '# Enter the paths where you want to copy the compiled output, each on a new line.\nExample:\n/path/to/destination1/node_modules/@onecx\n/path/to/destination2/node_modules/@onecx\n'
+  );
+  console.log(
+    '.copy-build-to file created. Please add the paths where you want to copy the compiled output, each on a new line.'
+  );
   process.exit(0);
 }
 
 // Read the paths from the .copy-build-to file
-const paths = fs.readFileSync(copyBuildToFile, 'utf-8').split('\n').filter(Boolean);
+const paths = fs
+  .readFileSync(copyBuildToFile, 'utf-8')
+  .split('\n')
+  .filter((c) => !c.startsWith('#'));
 
 // Define the source directory
 const sourceDir = path.join(__dirname, 'dist');
 
 // Copy the compiled output to each path
-paths.forEach(destination => {
+paths.forEach((destination) => {
   const destDir = path.resolve(destination);
   execSync(`cp -r ${sourceDir}/nx-plugin ${destDir}`, { stdio: 'inherit' });
   console.log(`Copied compiled output to ${destDir}`);
