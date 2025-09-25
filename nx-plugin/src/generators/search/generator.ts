dceb80c84ed1fb016a225655e50934956e60a1a1
Benjamin <166110368+BenjaminPabst@users.noreply.github.com>
2025-09-25 13:58:19 +0200

    docs: rewrite documentation (#280)
    * docs: added explanations
    
    * docs: refactored delete dialog docs
    
    * docs: refactor first part of search page
    
    * docs: finish search results and details page main
    
    * docs: finish search page docs
    
    * docs: improve structure, refactor create-update
    
    * docs: explanations => glossary, removed standalone
    
    * docs: removed standalone
    
    * feat: rename dataObjectName to Resource to match docs
    
    * docs: adapted to feedback

--- nx-plugin/src/generators/search/generator.ts
+++ nx-plugin/src/generators/search/generator.ts
@@ -55,7 +55,7 @@ const PARAMETERS: GeneratorParameter<SearchGeneratorSchema>[] = [
     default: (values) => {
       return `${names(values.featureName).className}`;
     },
-    prompt: 'Provide a name for your Data Object (e.g., Book): ',
+    prompt: 'Provide a name for your Resource (e.g., Book): ',
     showInSummary: true,
     showRules: [{ showIf: (values) => values.customizeNamingForAPI }],
   },
