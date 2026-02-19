0bb631faf88ee199146028c443ff19f1e7310be4
Henry Taeschner <github@henry-taeschner.de>
2026-02-19 10:17:52 +0100

    fix: linting

--- nx-plugin/src/generators/angular/files/proxy.conf.js
+++ nx-plugin/src/generators/angular/files/proxy.conf.js
@@ -2,10 +2,11 @@
  * Used in local environment:
  *   Request proxy to rewrite URLs and prevent CORS errors
  */
-const logFn = function (req, res) {
-  //console.log(new Date().toISOString() + `: bypassing ${req.method} ${req.url} `)
+// @typescript-eslint/no-unused-vars
+const logFn = function (_req, _res) {
+  //console.log(new Date().toISOString() + `: bypassing ${_req.method} ${_req.url} `)
 }
-const onProxyRes = function (proxyRes, req, res) {
+const onProxyRes = function (_proxyRes, req, res) {
   logFn(req, res)
   if (req.method.toUpperCase() === 'OPTIONS') {
     res.setHeader('Allow', 'GET, POST, HEAD, PUT, DELETE, OPTIONS')
